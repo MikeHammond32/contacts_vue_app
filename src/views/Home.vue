7d8695514e1a848f300350b45f6eead3a5f2cef8
Mike <mikehammond32@gmail.com>
2019-08-01 22:30:25 -0400

    mostly fleshed out create action and added vue template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+      <p>FirstName: <input type="text" v-model="newContactFirstName"></p>
+   <p>last_name: <input type="text" v-model="newContactLastName"></p>
+   <p>PhoneNumber: <input type="text" v-model="newContactPhoneNumber"></p>
+   <p>Email: <input type="text" v-model="newContactemail"></p>
+   <button v-on:click="makeContact()">Make a new contact</button>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "change.js",
+      contacts: [],
+      newContactFirstName: "",
+      newContactLastName: "",
+      newContactPhoneNumber: "",
+      newContactEmail: "",
+      currentContact: {}
+
+    };
+  },
+  created: function() {
+    console.log('in created');
+    axios.get("/api/products").then(response => {
+      console.log(response.data);
+      this.contacts = response.data;
+    })
+    
+  },
+  methods: {
+    makeContact: function() {
+      console.log('in make Contact');
+      var newContact = {
+        First_name: this.newContactFirstName,
+        Last_name: this.newContactLastName,
+        Email: this.newContactEmail,
+        Phone_Number: this.newContactPhoneNumber
+      }
+    }
   }
-}
+};
 </script>
